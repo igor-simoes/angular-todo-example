ca50a58f287df1ead3f6cc9cfa8939c400da6c75
igorsimoes <iigor.simoes@gmail.com>
2015-09-06 17:30:28 -0300

    add and remove tasks works

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ angular.module('todoApp').controller('todoController', function ($scope) {
     $scope.tasks = [
         {name: "Market", day: "Thursday", hour: "13:00"},
         {name: "Academy", day: "Wednesday", hour: "19:00"},
-        {name: "School", day: "Friday", hour: "08:00"},
+        {name: "School", day: "Friday", hour: "08:00"}
     ];
 
+    $scope.newTask = function(task){
+        $scope.tasks.push(angular.copy(task));
+        delete task;
+    };
 
+    $scope.removeTasks = function(){
+        $scope.tasks = $scope.tasks.filter(function(task){
+            if (!task.selections === true) return task;
+        });
+    };
 });
