dc1b6da33d693af36f2f24ca8c32376243a6c2b5
igorsimoes <iigor.simoes@gmail.com>
2015-09-06 16:46:21 -0300

    tasks list

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 angular.module('todoApp', []);
 
 angular.module('todoApp').controller('todoController', function ($scope) {
-    
+    $scope.app = "todoApp";
+
+    $scope.tasks = [
+        {name: "Market", day: "Thursday", hour: "13:00"},
+        {name: "Academy", day: "Wednesday", hour: "19:00"},
+        {name: "School", day: "Friday", hour: "08:00"},
+    ];
+
+
 });
